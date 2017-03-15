12711fdc6788d436d73007afd31f72c18a20f10a
Redmine <redmine@Elecard-SergA.elecard.net.ru>
2017-03-15 15:18:56 +0700

    Fix highlighting xml/html code by escaping html characters
    Signed-off-by: Redmine <redmine@Elecard-SergA.elecard.net.ru>

--- lib/highlightjs_highlighting.rb
+++ lib/highlightjs_highlighting.rb
@@ -2,11 +2,11 @@ module HighlightJsSyntaxHighlighting
   class << self
     def highlight_by_filename(text, filename)
       Rails.logger.info "redmine_highlightjs: syntax by filename #{filename}"  
-      return text
+      return ERB::Util.h(text)
     end
     def highlight_by_language(text, language)
       Rails.logger.info "redmine_highlightjs: syntax #{language}"
-      return text
+      return ERB::Util.h(text)
     end
   end
 end
